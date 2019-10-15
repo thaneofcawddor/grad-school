c07db484d7b49636376ceb3f867caeb0877da78c
Thane Richard <thanetrichard@gmail.com>
2019-10-15 15:42:38 -0600

    removed colons and replaced with curly brackets because this aint Python

--- ESOF 322 HW/duck_practice.java
+++ ESOF 322 HW/duck_practice.java
@@ -1,16 +1,25 @@
-public abstract class Car():
+public abstract class Car{
      EngineBehavior engineBehavior;
      FuelBehavior fuelBehavior;
      
-     public Car():
-          public void performEngineRev():
+     public void performEngineRev(){
+         EngineBehavior.rev()
+     }
           
+}
           
 public Interface EngineBehavior():
      public void rev();
      
      
-public Class ElectricMotor implements EngineBehavior():
-     public void rev():
+public Class ElectricMotor implements EngineBehavior(){
+     public void rev(){
           System.out.println("<<silence>>")
-          
+     }
+}
+
+public Class DieselEngine implements EngineBehavior(){
+     public void rev(){
+          System.out.println("VROOOOOOOM!!! I'm a Cummins, bitch!")
+     }
+}
